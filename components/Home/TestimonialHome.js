3ed7dcdb3b5f7a3283bf10b035513253aaa66926
Tuwan Ashnaz Sadiyan <ashnassadiyan@gmail.com>
2023-05-08 14:10:22 +0530

    testimonials are fixed

--- components/Home/TestimonialHome.js
+++ components/Home/TestimonialHome.js
@@ -9,6 +9,8 @@ import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import Carousel from "react-elastic-carousel";
+import Image from "next/image";
+import { useRef } from "react";
 
 const TestimonialHome = () => {
   const breakPoints = [
@@ -16,6 +18,10 @@ const TestimonialHome = () => {
     { width: 768, itemsToShow: 1 },
     { width: 974, itemsToShow: 1 },
   ];
+
+  const carouselRef = useRef(null);
+  const totalPages = 3;
+  let resetTimeout;
 
   return (
     <Container>
@@ -40,7 +46,20 @@ const TestimonialHome = () => {
           Testimonial
         </CustomTypography>
       </Stack>
-      <Carousel breakPoints={breakPoints}>
+      <Carousel
+        breakPoints={breakPoints}
+        enableAutoPlay
+        ref={carouselRef}
+        showArrows={false}
+        onNextEnd={({ index }) => {
+          clearTimeout(resetTimeout);
+          if (index + 1 === totalPages) {
+            resetTimeout = setTimeout(() => {
+              carouselRef.current.goTo(0);
+            }, 1500); // same time
+          }
+        }}
+      >
         <Box
           sx={{
             backgroundImage: "url(/union.png)",
@@ -75,11 +94,142 @@ const TestimonialHome = () => {
                 Lorem ipsum
               </CustomTypography>
               <Stack direction="row" spacing={1}>
-                <img src="/Star.png" alt="" />
-                <img src="/Star.png" alt="" />
-                <img src="/Star.png" alt="" />
-                <img src="/Star.png" alt="" />
-                <img src="/Star.png" alt="" />
+                <Image src="/Star.png" alt="" width={25} height={25} />
+                <Image src="/Star.png" alt="" width={25} height={25} />
+                <Image src="/Star.png" alt="" width={25} height={25} />
+                <Image src="/Star.png" alt="" width={25} height={25} />
+                <Image src="/Star.png" alt="" width={25} height={25} />
+              </Stack>
+            </Grid>
+            <Grid
+              item
+              md={9}
+              xs={12}
+              sm={12}
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "column",
+                minHeight: "200px",
+                padding: "40px 30px",
+                top: { md: "0", xs: "-100px", sm: "-100px" },
+                position: "relative",
+              }}
+            >
+              <CustomTypography sx={{ textAlign: "justify", fontSize: "18px" }}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+                blanditiis tenetur unde suscipit, quam beatae rerum inventore
+                consectetur, neque doloribus, cupiditate numquam dignissimos
+                laborum fugiat deleniti? Eum quasi quidem quibusdam.
+              </CustomTypography>
+            </Grid>
+          </Grid>
+        </Box>
+
+        <Box
+          sx={{
+            backgroundImage: "url(/union.png)",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "100% 100%",
+            marginTop: "150px",
+            borderColor: "#034275",
+            borderStyle: "solid",
+          }}
+        >
+          <Grid container>
+            <Grid
+              item
+              md={3}
+              xs={12}
+              sm={12}
+              sx={{
+                display: "flex",
+                justifyItems: "center",
+                alignItems: "center",
+                flexDirection: "column",
+                minHeight: "200px",
+                padding: "25px",
+                top: "-100px",
+                position: "relative",
+              }}
+            >
+              <Box>
+                <Avatar sx={{ width: 130, height: 130 }} />
+              </Box>
+              <CustomTypography sx={{ fontSize: "28px", fontWeight: "600" }}>
+                Lorem ipsum
+              </CustomTypography>
+              <Stack direction="row" spacing={1}>
+                <Image src="/Star.png" alt="" width={25} height={25} />
+                <Image src="/Star.png" alt="" width={25} height={25} />
+                <Image src="/Star.png" alt="" width={25} height={25} />
+                <Image src="/Star.png" alt="" width={25} height={25} />
+                <Image src="/Star.png" alt="" width={25} height={25} />
+              </Stack>
+            </Grid>
+            <Grid
+              item
+              md={9}
+              xs={12}
+              sm={12}
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "column",
+                minHeight: "200px",
+                padding: "40px 30px",
+                top: { md: "0", xs: "-100px", sm: "-100px" },
+                position: "relative",
+              }}
+            >
+              <CustomTypography sx={{ textAlign: "justify", fontSize: "18px" }}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+                blanditiis tenetur unde suscipit, quam beatae rerum inventore
+                consectetur, neque doloribus, cupiditate numquam dignissimos
+                laborum fugiat deleniti? Eum quasi quidem quibusdam.
+              </CustomTypography>
+            </Grid>
+          </Grid>
+        </Box>
+        <Box
+          sx={{
+            backgroundImage: "url(/union.png)",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "100% 100%",
+            marginTop: "150px",
+            borderColor: "#034275",
+            borderStyle: "solid",
+          }}
+        >
+          <Grid container>
+            <Grid
+              item
+              md={3}
+              xs={12}
+              sm={12}
+              sx={{
+                display: "flex",
+                justifyItems: "center",
+                alignItems: "center",
+                flexDirection: "column",
+                minHeight: "200px",
+                padding: "25px",
+                top: "-100px",
+                position: "relative",
+              }}
+            >
+              <Box>
+                <Avatar sx={{ width: 130, height: 130 }} />
+              </Box>
+              <CustomTypography sx={{ fontSize: "28px", fontWeight: "600" }}>
+                Lorem ipsum
+              </CustomTypography>
+              <Stack direction="row" spacing={1}>
+                <Image src="/Star.png" alt="" width={25} height={25} />
+                <Image src="/Star.png" alt="" width={25} height={25} />
+                <Image src="/Star.png" alt="" width={25} height={25} />
+                <Image src="/Star.png" alt="" width={25} height={25} />
+                <Image src="/Star.png" alt="" width={25} height={25} />
               </Stack>
             </Grid>
             <Grid
