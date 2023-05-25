bc81248b5a7cf1705b8fa517a7215f0fdcb121cd
Tuwan Ashnaz Sadiyan <ashnassadiyan@gmail.com>
2023-05-25 14:36:01 +0530

    emotions moved

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 import createEmotionServer from "@emotion/server/create-instance";
-import createEmotionCache from "./createEmotionCache";
+import createEmotionCache from "../theme/createEmotionCache";
 
 export default class MyDocument extends Document {
   render() {
