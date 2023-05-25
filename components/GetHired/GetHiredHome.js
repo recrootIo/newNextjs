bb9d1654c2aac4d4596531117206087ca63d3d31
Tuwan Ashnaz Sadiyan <ashnassadiyan@gmail.com>
2023-05-25 11:22:52 +0530

    cursor fixed

--- components/GetHired/GetHiredHome.js
+++ components/GetHired/GetHiredHome.js
@@ -101,10 +101,10 @@ const GetHiredHome = () => {
             depth: 100,
             modifier: 2.5,
           }}
-          // autoplay={{
-          //   delay: 2500,
-          //   disableOnInteraction: false,
-          // }}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
           pagination={{ el: ".swiper-pagination", clickable: true }}
           navigation={{
             nextEl: ".swiper-next",
