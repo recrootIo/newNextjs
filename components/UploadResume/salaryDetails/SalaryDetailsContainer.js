58d2c7703cec4c686a140ab9034cb78b6bf9fcae
Tuwan Ashnaz Sadiyan <ashnassadiyan@gmail.com>
2023-05-25 11:35:37 +0530

    exported meta removed

--- components/UploadResume/salaryDetails/SalaryDetailsContainer.js
+++ components/UploadResume/salaryDetails/SalaryDetailsContainer.js
@@ -2,10 +2,6 @@ import { ParallaxLayer } from "@react-spring/parallax";
 import React from "react";
 import Salary from "./salarydetails";
 
-export const metadata = {
-  title: "Salary Details",
-};
-
 const SalaryDetailsContainer = ({ ...props }) => {
   const { position } = props;
   return (
