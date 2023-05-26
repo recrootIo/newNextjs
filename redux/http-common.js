83a8a244a216d1984d9be1f2ced77624443f2bd8
Tuwan Ashnaz Sadiyan <ashnassadiyan@gmail.com>
2023-05-26 20:48:55 +0530

    checking api

--- redux/http-common.js
+++ redux/http-common.js
@@ -2,14 +2,14 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 const http = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "https://Preprod.recroot.au/api/",
   headers: {
     "Content-type": "application/json",
   },
 });
 
 http.interceptors.request.use((config) => {
-  const token = Cookies.get('token')
+  const token = Cookies.get("token");
   config.headers = config.headers || {};
   config.headers["x-access-token"] = token;
 
