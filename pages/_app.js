f5c6ad3c3867118cf1c938341bf0d55b29e4b565
Tuwan Ashnaz Sadiyan <ashnassadiyan@gmail.com>
2023-05-26 16:40:10 +0530

    new server changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,31 @@
 import "./globals.css";
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import {
+  // CssBaseline
+  //,
+  ThemeProvider,
+} from "@mui/material";
 import { darkTheme } from "../redux/slices/theme";
 import { Provider } from "react-redux";
 import { Rstore } from "@/redux/store/store";
 import Alerts from "@/ui-components/Alerts/Alerts";
+import createEmotionCache from "@/theme/createEmotionCache";
+import { CacheProvider } from "@emotion/react";
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps,
+  emotionCache = createEmotionCache,
+}) {
   return (
-    <Provider store={Rstore}>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline>
+    <CacheProvider value={emotionCache}>
+      <Provider store={Rstore}>
+        <ThemeProvider theme={darkTheme}>
+          {/* <CssBaseline> */}
           <Component {...pageProps} />
           <Alerts />
-        </CssBaseline>
-      </ThemeProvider>
-    </Provider>
+          {/* </CssBaseline> */}
+        </ThemeProvider>
+      </Provider>
+    </CacheProvider>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 import createEmotionServer from "@emotion/server/create-instance";
-import createEmotionCache from "../theme/createEmotionCache";
+import createEmotionCache from "@/theme/createEmotionCache";
 
 export default class MyDocument extends Document {
   render() {
@@ -27,14 +27,8 @@ export default class MyDocument extends Document {
   }
 }
 
-// `getInitialProps` belongs to `_document` (instead of `_app`),
-// it's compatible with static-site generation (SSG).
 MyDocument.getInitialProps = async (ctx) => {
   const originalRenderPage = ctx.renderPage;
-
-  // You can consider sharing the same emotion cache between
-  // all the SSR requests to speed up performance.
-  // However, be aware that it can have global side effects.
 
   const cache = createEmotionCache();
   const { extractCriticalToChunks } = createEmotionServer(cache);
@@ -49,16 +43,11 @@ MyDocument.getInitialProps = async (ctx) => {
 
   const initialProps = await Document.getInitialProps(ctx);
 
-  // This is important. It prevents emotion to render invalid HTML.
-  // See
-  // https://github.com/mui-org/material-ui/issues/26561#issuecomment-855286153
-
   const emotionStyles = extractCriticalToChunks(initialProps.html);
   const emotionStyleTags = emotionStyles.styles.map((style) => (
     <style
       data-emotion={`${style.key} ${style.ids.join(" ")}`}
       key={style.key}
-      // eslint-disable-next-line react/no-danger
       dangerouslySetInnerHTML={{ __html: style.css }}
     />
   ));
