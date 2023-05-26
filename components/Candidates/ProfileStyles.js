7be04e5fa96f4f3902c1554a122e7a464047b8ba
recrootIo <105347584+recrootIo@users.noreply.github.com>
2023-05-26 15:42:34 +0530

    Revert "new changes"

--- components/Candidates/ProfileStyles.js
+++ components/Candidates/ProfileStyles.js
@@ -1,4 +1,5 @@
-import { Card, styled } from "@mui/material";
+import { Card } from "@mui/material";
+import styled from "styled-components";
 
 const StyledCard = styled(Card)({
   border: "1px solid #D3EAFF",
