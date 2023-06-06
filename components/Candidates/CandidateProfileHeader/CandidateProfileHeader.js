7d4cde13db19516a1d0216a3fa1b70ab505b0861
Tuwan Ashnaz Sadiyan <ashnassadiyan@gmail.com>
2023-06-06 10:31:47 +0530

    finalized the percentage

--- components/Candidates/CandidateProfileHeader/CandidateProfileHeader.js
+++ components/Candidates/CandidateProfileHeader/CandidateProfileHeader.js
@@ -9,6 +9,7 @@ import {
   Typography,
   Dialog,
   Skeleton,
+  Stack,
   // Avatar
 } from "@mui/material";
 import { CustomTypography } from "@/ui-components/CustomTypography/CustomTypography";
@@ -18,7 +19,12 @@ import PhoneOutlinedIcon from "@mui/icons-material/PhoneOutlined";
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import LocalAtmOutlinedIcon from "@mui/icons-material/LocalAtmOutlined";
 import { EditRounded } from "@mui/icons-material";
-import { addProfphoto, retrievePersonal } from "@/redux/slices/personal";
+import {
+  addProfphoto,
+  getPercentage,
+  retrievePersonal,
+  updatePercent,
+} from "@/redux/slices/personal";
 import { UploadPhoto } from "@/utils/UploadPhoto";
 import { useDispatch, useSelector } from "react-redux";
 import { NEUTRAL } from "@/theme/colors";
@@ -37,6 +43,10 @@ const CandidateProfileHeader = (data) => {
     (state) => state.personal?.data?.profpicFileLocation
   );
 
+  const percent = useSelector((data) => data.personal.percentage);
+  const users = useSelector((state) => state.personal?.data?.resume);
+  const personal = useSelector((state) => state.personal?.data);
+
   const [fullWidth, setFullWidth] = React.useState(true);
   const [maxWidth, setMaxWidth] = React.useState("sm");
   const [open, setOpen] = React.useState(false);
@@ -44,8 +54,6 @@ const CandidateProfileHeader = (data) => {
   const [fileNames, setFileNames] = React.useState("");
   const [srcsjjj, setSrcsjjj] = React.useState("");
   const [loadimg, setloadimg] = React.useState(false);
-
-  console.log(data, "data");
 
   const dispatch = useDispatch();
 
@@ -90,9 +98,102 @@ const CandidateProfileHeader = (data) => {
         `https://api.arinnovate.io/api/openProfpic?photo=${photoss.photo}`
       );
     }
-  }, [photoss]);
+
+    dispatch(getPercentage());
+  }, [photoss, dispatch]);
+
+  const skill =
+    percent?.skills?.length <= users?.skills?.length
+      ? percent?.skills?.percent
+      : percent?.skills?.length / 2 === users?.skills?.length
+      ? percent?.skills?.percent / 2
+      : 0;
+
+  const Experience =
+    percent?.workExperince?.length <= users?.workExperience?.length
+      ? percent?.workExperince?.percent
+      : 0;
+
+  const Education =
+    percent?.education?.length <= users?.education?.length
+      ? percent?.education?.percent
+      : percent?.education?.length / 2 === users?.education?.length
+      ? percent?.education?.percent / 2
+      : 0;
+
+  const Certificate =
+    percent?.certificate?.length <= users?.certificateFileLocation?.length
+      ? percent?.certificate?.percent
+      : percent?.certificate?.length / 2 ===
+        users?.certificateFileLocation?.length
+      ? percent?.certificate?.percent / 2
+      : 0;
+
+  const persan =
+    (personal?.about !== null ? 1 : 0) +
+    (users?.languages?.length > 0 ? 1 : 0) +
+    (users?.currentOffer !== undefined ? 1 : 0) +
+    (personal?.jobTitle !== undefined ? 1 : 0) +
+    (personal?.mobile !== undefined && personal?.mobile !== null ? 1 : 0);
+
+  const persanRank =
+    persan === percent?.personalInfo?.personal?.length
+      ? percent?.personalInfo?.personal?.percent
+      : 0;
+
+  const countrs =
+    (users?.location?.country !== undefined ? 1 : 0) +
+    (users?.location?.state !== undefined ? 1 : 0) +
+    (users?.location?.city !== undefined ? 1 : 0) +
+    (users?.totalWorkExperience !== undefined ? 1 : 0);
+
+  const countries =
+    countrs === percent?.personalInfo?.countries?.length
+      ? percent?.personalInfo?.countries?.percent
+      : 0;
+
+  const essen =
+    (users?.notice !== undefined ? 1 : 0) +
+    (users?.expectedSalary !== undefined ? 1 : 0) +
+    (users?.currentSalary !== undefined ? 1 : 0) +
+    (users?.salaryCurrency !== undefined ? 1 : 0) +
+    (users?.workPrefence !== undefined ? 1 : 0);
+
+  const essential =
+    essen === percent?.personalInfo?.essent?.length
+      ? percent?.personalInfo?.essent?.percent
+      : 0;
+
+  const finalPercent =
+    skill +
+    Experience +
+    Education +
+    Certificate +
+    // Social +
+    persanRank +
+    countries +
+    essential;
+
+  console.log(finalPercent, "finalPercent");
 
   const imageUrl = photoss?.photo ? srcsjjj : "";
+
+  const addPercent = () => {
+    const loggedInUser = JSON.parse(localStorage.getItem("User"));
+    loggedInUser.User.profilePercentage = finalPercent;
+    localStorage.setItem("User", JSON.stringify(loggedInUser));
+    dispatch(updatePercent(finalPercent));
+  };
+
+  React.useEffect(() => {
+    addPercent();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    percent?.skills,
+    percent?.workExperince,
+    percent?.education,
+    percent?.certificate,
+  ]);
 
   return (
     <Box
@@ -259,6 +360,7 @@ const CandidateProfileHeader = (data) => {
                   backgroundRepeat: "no-repeat",
                   backgroundSize: "contain",
                   height: "200px",
+                  flexWrap: "wrap",
                 }}
               >
                 <CustomTypography
@@ -267,29 +369,54 @@ const CandidateProfileHeader = (data) => {
                     position: "absolute",
                     fontFamily: "Inter-bold",
                     zIndex: "1",
+                    fontSize: "2rem",
                     top: "50%",
                     left: "50%",
                     transform: "translate(-50%, -50%)",
                   }}
                 >
-                  {data?.profilePercentage}
+                  {data?.profilePercentage}%
                 </CustomTypography>
 
                 <CustomTypography
                   variant="subtitle1"
                   sx={{
-                    mt: "25px",
-                    fontSize: "14px",
+                    mt: "20px",
+                    fontSize: "17px",
                     position: "relative",
-                    top: "20px",
+                    top: "50px",
                     color: NEUTRAL,
+                    flexWrap: "wrap",
+                    width: "200px",
                   }}
                 >
-                  Profile completed (Excellent)
+                  {/* Enhance your profile by adding more information to aim for a
+                  minimum of 70%. */}
                 </CustomTypography>
               </Box>
             )}
           </Grid>
+          {data?.profilePercentage < 70 && (
+            <Grid md={12}>
+              <Stack direction="row" sx={{ justifyContent: "flex-end" }}>
+                <CustomTypography
+                  variant="subtitle1"
+                  sx={{
+                    fontSize: "17px",
+                    position: "relative",
+                    color: NEUTRAL,
+                    flexWrap: "wrap",
+                    width: { md: "300px", xs: "100%", sm: "100%" },
+                    textAlign: "right",
+                    fontWeight: "900",
+                  }}
+                >
+                  Enhance your profile by adding more information to aim for a
+                  minimum of 70%.
+                </CustomTypography>
+              </Stack>
+            </Grid>
+          )}
         </Grid>
 
         <Dialog
---
+++ utils/profileCompletion.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const profileCompletion = () => {
+  return <div>profileCompletion</div>;
+};
+
+export default profileCompletion;
