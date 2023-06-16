043876acda6fe7d2eac44aca7bdd62cb8d91cf4b
Tuwan Ashnaz Sadiyan <ashnassadiyan@gmail.com>
2023-06-16 16:30:00 +0530

    subscribe

--- pages/resetPassword/[id]/[token]/index.js
+++ pages/resetPassword/[id]/[token]/index.js
@@ -20,7 +20,7 @@ export const getServerSideProps = async (context) => {
 
   try {
     const response = await axios.get(
-      `http://localhost:3000/reset-password/${id}/${token}`
+      `https://preprod.recroot.au/reset-password/${id}/${token}`
     );
     if (response.data === "NotVerified") {
       context.res.writeHead(302, { Location: "/forgotPass" });
