b4d7d3a5a7006ddf0757c1cbbecaba40395295fe
madhanArinno <madhan@arinnovate.io>
2023-06-19 11:34:03 +0530

    loading-animation-added

--- pages/_app.js
+++ pages/_app.js
@@ -5,17 +5,68 @@ import { Provider } from "react-redux";
 import { Rstore } from "@/redux/store/store";
 import Alerts from "@/ui-components/Alerts/Alerts";
 import { Inter } from "next/font/google";
-
+import React, { useEffect, useState } from "react";
+import { useRouter } from 'next/router';
+import Loader from '@/components/Loader/Loader'
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({ subsets: ["cyrillic"] });
 
 export default function App({ Component, pageProps }) {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChangeStart = () => {
+      setLoading(true);
+    };
+
+    const handleRouteChangeComplete = () => {
+      setLoading(false);
+    };
+
+    router.events.on('routeChangeStart', handleRouteChangeStart);
+    router.events.on('routeChangeComplete', handleRouteChangeComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChangeStart);
+      router.events.off('routeChangeComplete', handleRouteChangeComplete);
+    };
+  }, [router]);
   return (
     <Provider store={Rstore}>
       <ThemeProvider theme={darkTheme}>
         <CssBaseline>
-          <Component {...pageProps} className={inter.className} />
+        <div className={`page-container ${loading ? 'blur' : ''}`}>
+         <Component {...pageProps} className={inter.className} />
+        </div>
+        {loading && (
+        <div className="loading-overlay">
+          <Loader />
+        </div>
+      )}
           <Alerts />
+          <style jsx global>{`
+        .page-container {
+          position: relative;
+        }
+
+        .blur {
+          filter: blur(4px);
+          pointer-events: none;
+        }
+        .loading-overlay {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          background-color: rgba(0, 0, 0, 0.5);
+          z-index: 2;
+        }
+      `}</style>
         </CssBaseline>
       </ThemeProvider>
     </Provider>
