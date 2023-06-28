eecc10293ec232f2738ed15a4a0991d2f3ca9d6d
Tuwan Ashnaz Sadiyan <ashnassadiyan@gmail.com>
2023-06-28 13:02:04 +0530

    checking employer

--- pages/_app.js
+++ pages/_app.js
@@ -29,10 +29,19 @@ export default function App({ Component, pageProps }) {
         if (country === "LK" && !router.asPath.startsWith("/lk/")) {
           const excluded = ["/lk/Employer/Dashboard"];
           const newPaths = `/lk${router.asPath}`;
+
           if (excluded.includes(newPaths) || router.asPath === "/") {
-            router.push(`/${router.asPath}`);
+            if (newPaths === "/lk/Employer/Dashboard") {
+              router.push(`/Employer/Dashboard`);
+            } else {
+              router.push(`/${router.asPath}`);
+            }
           } else {
-            router.push(newPaths);
+            if (newPaths === "/lk/Employer/Dashboard") {
+              router.push(`/Employer/Dashboard`);
+            } else {
+              router.push(`/${router.asPath}`);
+            }
           }
         }
 
