3d6ed79f1f4a3faef8ba112422b8718792bfdf1e
madhanArinno <madhan@arinnovate.io>
2023-07-04 17:29:05 +0530

    document change

--- pages/_document.js
+++ pages/_document.js
@@ -20,14 +20,14 @@ export default class MyDocument extends Document {
         </Head>
                  <script type="text/javascript">
             var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
+            {/* (function(){
             var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
             s1.async=true;
             s1.src='https://embed.tawk.to/6468b6b3ad80445890ee10c8/1h0sgjcd0';
             s1.charset='UTF-8';
             s1.setAttribute('crossorigin','*');
             s0.parentNode.insertBefore(s1,s0);
-            })();
+            })(); */}
           </script>
         <body>
           <NextScript />
