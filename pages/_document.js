c72acce130a2cd48c27db5022f995650651492a7
recrootIo <105347584+recrootIo@users.noreply.github.com>
2023-07-04 17:38:09 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -18,17 +18,17 @@ export default class MyDocument extends Document {
           {/* Inject MUI styles first to match with the prepend: true configuration. */}
           {this.props.emotionStyleTags}
         </Head>
-                 <script type="text/javascript">
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/6468b6b3ad80445890ee10c8/1h0sgjcd0';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-            })();
-          </script>
+          //        <script type="text/javascript">
+          //   var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+          //   (function(){
+          //   var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+          //   s1.async=true;
+          //   s1.src='https://embed.tawk.to/6468b6b3ad80445890ee10c8/1h0sgjcd0';
+          //   s1.charset='UTF-8';
+          //   s1.setAttribute('crossorigin','*');
+          //   s0.parentNode.insertBefore(s1,s0);
+          //   })();
+          // </script>
         <body>
           <NextScript />
           <Main />
