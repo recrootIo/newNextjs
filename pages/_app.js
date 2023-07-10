b0588a901a17ba89170fb07324832a468f24263c
dilanja <105347584+recrootIo@users.noreply.github.com>
2023-07-10 13:50:09 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -136,6 +136,19 @@ const App = ({ Component, pageProps }) => {
           gtag('config', 'G-7F7LTF5XJN');
         `}
       </Script>
+      <Script strategy="afterInteractive" id="google-analytics">
+        {`
+           var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+           (function(){
+           var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+           s1.async=true;
+           s1.src='https://embed.tawk.to/6468b6b3ad80445890ee10c8/1h0sgjcd0';
+           s1.charset='UTF-8';
+           s1.setAttribute('crossorigin','*');
+           s0.parentNode.insertBefore(s1,s0);
+           })();
+        `}
+      </Script>
     </Provider>
   );
 };
