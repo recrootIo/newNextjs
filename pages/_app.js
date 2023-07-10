f74a9c49173d86af0ccf2a276eb8ef626189fbad
dilanja <105347584+recrootIo@users.noreply.github.com>
2023-07-10 13:46:24 +0530

    google

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ import Loader from "@/components/Loader/Loader";
 import cookies from "js-cookie";
 import axios from "axios";
 import { appWithTranslation } from "next-i18next";
-
+import Script from "next/script";
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({ subsets: ["cyrillic"] });
 
--- pages/_document.js
+++ pages/_document.js
@@ -17,28 +17,8 @@ export default class MyDocument extends Document {
           /> */}
           {/* Inject MUI styles first to match with the prepend: true configuration. */}
           {this.props.emotionStyleTags}
-    { <script async src="https://www.googletagmanager.com/gtag/js?id=G-7F7LTF5XJN"></script>}
-     { <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-          
-            gtag('config', 'G-7F7LTF5XJN');
-          </script>}
-          <!--Start of Tawk.to Script-->
-            {  <script type="text/javascript">
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/6468b6b3ad80445890ee10c8/1h0sgjcd0';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-            })();
-            </script>}
         </Head>
-          {/* //        <script type="text/javascript">
+        {/* //        <script type="text/javascript">
           //   var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
           //   (function(){
           //   var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
